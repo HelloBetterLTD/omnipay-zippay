42278f33544f80e9ed2d06ef3d1a094b0ba935b8
Nivanka Fonseka <nivanka@silverstripers.com>
2018-08-18 09:38:52 +0530

    debug logs and developing the module

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -19,13 +19,8 @@ class PurchaseRequest extends AbstractRequest
 
 	public function getData()
 	{
-		$checkout = new Checkout();
-		$checkout->request->charge = false;
-		$checkout->request->currency_code = $this->getCurrency();
-		$checkout->request->txn_id = false;
-		$checkout->request->order_id =  $this->getTestMode();
-		$checkout->request->in_store = false;
 
+		/*
 //		$checkout->request->cart_url    = "https://your-domain/checkout/cart/";
 //		$checkout->request->success_url = $this->succ;
 //		$checkout->request->cancel_url  = "https://your-domain/zipmoney/express/cancel/";
@@ -58,11 +53,49 @@ class PurchaseRequest extends AbstractRequest
 
 		die();
 
+		*/
 	}
 
 	public function sendData($data)
 	{
+
+
+		echo '<pre>' . print_r($data, 1) . '</pre>';
+		die();
+
+		$checkout = new Checkout();
+		$checkout->request->charge = false;
+		$checkout->request->currency_code = "AUD";
+		$checkout->request->txn_id = false;
+		$checkout->request->order_id =  $this->getTransactionId();
+		$checkout->request->in_store = false;
+
+
+		// $checkout->request->cart_url    = "https://your-domain/checkout/cart/";
+		$checkout->request->success_url = $this->getReturnUrl(); //"https://your-domain/checkout/success/";
+		$checkout->request->cancel_url  = $this->getCancelUrl(); "https://your-domain/zipmoney/express/cancel/";
+		$checkout->request->error_url   = $this->getReturnUrl(); //"https://your-domain/zipmoney/express/error/";
+		//$checkout->request->refer_url   = "https://your-domain/zipmoney/express/refer/";
+		$checkout->request->decline_url = $this->getReturnUrl(); //"https://your-domain/zipmoney/express/decline/";
+
+		$order = new Order();
+		$order->id = $orderID;
+		$order->tax = 1;
+		$order->shipping_tax = 0;
+		$order->shipping_value = 1;
+		$order->total = 2;
+
+
+
+		$checkout = new Checkout();
+		$checkout->request->charge = false;
+		$checkout->request->currency_code = $this->getCurrency();
+		$checkout->request->txn_id = false;
+		$checkout->request->order_id =  $this->getTestMode();
+		$checkout->request->in_store = false;
+
 		return $this->response = new PurchaseResponse($this, $data);
 	}
+
 
 }
